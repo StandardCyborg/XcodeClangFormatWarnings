c36ac2a93a44a965f02e1b944cd4a5bea1d0b77c
Aaron Thompson <aaron@standardcyborg.com>
2019-08-29 16:58:51 -0700

    Fixes mismatch between clang-format's byte offsets and Python 3's unicode character offsets

--- run-clang-format.py
+++ run-clang-format.py
@@ -73,14 +73,13 @@ def run_clang_format_on_file(absolute_filename):
         file_string = file.read()
     
     for replacement in replacements_xml_tree:
-        replacement_offset = int(replacement.attrib["offset"])
+        replacement_offset_bytes = int(replacement.attrib["offset"])
         replacement_length = int(replacement.attrib["length"])
-        replacement_line_number, replacement_column = line_number_from_offset(file_string, replacement_offset)
+        replacement_line_number, replacement_column, replacement_offset = line_number_from_offset(file_string, replacement_offset_bytes)
         replacement_text = replacement.text or ""
+        original_text = file_string[replacement_offset:replacement_offset+replacement_length]
         
-        # Special rule: avoid trimming trailing whitespace from empty lines
-        # http://clang-developers.42468.n3.nabble.com/clang-format-leading-whitespace-td4058643.html
-        if replacement_text.startswith("\n\n    ") and file_string[replacement_offset-1] == '\n':
+        if should_ignore_replacement(original_text, replacement_text):
             continue
         
         # Make the replacement text easier to read
@@ -100,33 +99,53 @@ def run_clang_format_on_file(absolute_filename):
         print(warning_header + warning_message + warning_message_details)
 
 
-def line_number_from_offset(file_string, offset):
+def should_ignore_replacement(original_text, replacement_text):
+    # Special rule: avoid trimming trailing whitespace from empty lines
+    # http://clang-developers.42468.n3.nabble.com/clang-format-leading-whitespace-td4058643.html
+    if original_text.startswith("\n    ") and replacement_text.startswith("\n\n"):
+        return True
+    
+    return False
+
+
+def line_number_from_offset(file_string, byte_offset):
     line_number = 1
     column = 0
+    i = 0
+    # clang-format uses bytes, not characters, as offsets and lengths, so we'll
+    # keep track of the difference here
+    character_offset = byte_offset
     
-    for i in range(offset):
-        if file_string[i] == '\n':
+    while i < character_offset:
+        character = file_string[i]
+        character_byte_count = len(bytes(character, 'utf8'))
+        
+        if character == '\n':
             line_number += 1
             column = 0
         else:
             column += 1
+        
+        character_offset -= character_byte_count - 1
+        i += 1
     
-    return line_number, column
+    return line_number, column, character_offset
+
 
 def build_warning_message(replacement_text, replacement_length, replacement_offset, file_string):
     if not replacement_text:
         if replacement_length == 1:
-            return "remove space" if file_string[replacement_offset-1] == ' ' else "remove character"
+            return "remove space" if file_string[replacement_offset] == ' ' else "remove character"
         else:
             return "remove next {} chars".format(replacement_length)
     else:
         if replacement_length == 0:
             return "add space" if replacement_text == ' ' else "add \"{}\"".format(replacement_text)
         elif replacement_length == 1:
-            return "replace {} with \"{}\"".format("newline" if file_string[replacement_offset-1] == '\n' else  "char", replacement_text)
+            return "replace {} with \"{}\"".format("newline" if file_string[replacement_offset] == '\n' else  "char", replacement_text)
         elif replacement_length == 2 and replacement_text == ' ': # Not necessarily accurate, but most likely
             return "remove a space"
-        elif file_string[replacement_offset-1] == "\n" and replacement_length == 1:
+        elif file_string[replacement_offset] == "\n" and replacement_length == 1:
             return "remove a newline"
         elif replacement_text.find("#include") != -1 and replacement_length > len("#include "):
             return "alphabetize headers"
@@ -139,11 +158,11 @@ def build_warning_message_details(replacement_text, replacement_length, replacem
     surrounding_text_end = min(len(file_string), replacement_offset + surrounding_character_count)
     
     surrounding_text_with_replacement = \
-        file_string[surrounding_text_start:replacement_offset-1] + \
+        file_string[surrounding_text_start:replacement_offset] + \
         replacement_text + \
-        file_string[replacement_offset-1+replacement_length:surrounding_text_end+replacement_length]
+        file_string[replacement_offset+replacement_length:surrounding_text_end+replacement_length]
     
-    return "  ➡️  …" + surrounding_text_with_replacement
+    return "  ➡️  …" + surrounding_text_with_replacement.replace("\n", "\\n")
 
 def get_git_modified_files():
     result = []
@@ -174,3 +193,4 @@ if __name__ == "__main__":
         path = os.getcwd()
     
     run_clang_format(path, should_apply_fixes)
+
